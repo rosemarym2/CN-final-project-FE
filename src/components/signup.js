a5a0765dcfd35e4fbb61f8d99ff9536c5c6f16ff
Ju1ija <j.lipnickaja@gmail.com>
2022-01-06 11:01:46 +0000

    created signup component

---
+++ src/components/signup.js
@@ -0,0 +1,31 @@
+import { useState } from "react";
+import { signUpFetch } from "../utils";
+
+export const SignUp = () => {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const signUpHandler = async () => {
+    // e.preventDefault();
+    const success = signUpFetch(username, email, password);
+    // if (success) {
+    //   history.push("/login");
+    // }
+  }
+
+  return (
+    <div className="signup-page" style={{ textAlign: "center" }}>
+      <form onSubmit={signUpHandler}>
+        <input onChange={(e) => setUsername(e.target.value)} autocapitalize="none" placeholder="Username" type="text" value={username} />
+        <input onChange={(e) => setEmail(e.target.value)} autocapitalize="none" placeholder="Email address" type="text" value={email} />
+        <div className="password-input">
+          <input onChange={(e) => setPassword(e.target.value)} autocapitalize="none" placeholder="Password" type="password" value={password} />
+          {/* <i class={passIcon} onClick={visiblePassHandler}></i> */}
+        </div>
+        <button type="submit">Sign Up</button>
+      </form>
+      {/* <p>Already have an account? <Link to="/login">Sign In</Link></p> */}
+    </div >
+  );
+}

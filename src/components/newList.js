047e35613be5a255d943c8ac8182d9a5f056f4d2
Holly Ford <holly_ford@hotmail.com>
2022-01-07 16:59:20 +0000

    completed add list component

--- src/components/newList.js
+++ src/components/newList.js
@@ -5,11 +5,23 @@ export const NewList = () => {
     const [title, setTitle] = useState("");
     const [category, setCategory] = useState("");
     const [keywords, setKeywords] = useState([]);
+    const [keywordStr, setKeywordStr] = useState("");
     const [listItems, setListItems] = useState([]);
     const [itemName, setItemName] = useState("");
     const [itemInfo, setItemInfo] = useState("");
 
+    const newListHandler = async () => {
+        const finalListItems = listItems.map((item) => ({
+            image: "",
+            itemName: item.itemName,
+            itemInfo: item.itemInfo,
+            completed: false
+        }));
+        const success = addNewListFetch(title, category, keywords, finalListItems);
+      }
+
     const newKeyword = (input) => {
+        console.log(input);
         const storedKeys = [...keywords]
         storedKeys.push(input)
         setKeywords(storedKeys)
@@ -18,19 +30,15 @@ export const NewList = () => {
     const nameChangeHandler = (event) => {
         setItemName(event.target.value);
       }
+      
       const infoChangeHandler = (event) => {
         setItemInfo(event.target.value);
       }
 
-    const newListItem = (input) => {
-        const storedName = [...itemName]
-        storedName.push(input)
-        setItemName(storedName)
-        const storedInfo = [...itemInfo]
-        storedInfo.push(input)
-        setItemInfo(storedInfo)
-        // const storedList = [...listItems]
-        // storedList.push(itemName, itemInfo)
+    const newListItem = (name, info) => {
+        const currentArr = [...listItems]
+        currentArr.push({itemName: name, itemInfo: info})
+        setListItems(currentArr);
     };
 
     return (
@@ -38,36 +46,35 @@ export const NewList = () => {
             <h1>Create a New List</h1>
             <h2>Set Up Your List</h2>
             <label for="listTitle">Name Your List</label>
-            <input type="text" onChange={(event) => setTitle(event.target.value)} id="listTitle" name="listTitle" placeholder="My New List" />
+            <input type="text" onChange={(event) => setTitle(event.target.value)} id="listTitle" name="listTitle" placeholder="My New List" required/>
 
             <label for="listCat">Category</label>
-            <input type="text" onChange={(event) => setCategory(event.target.value)} id="listCat" name="listCat" placeholder="Travel" />
+            <input type="text" onChange={(event) => setCategory(event.target.value)} id="listCat" name="listCat" placeholder="Travel" required/>
 
             <label for="listKeyword">Keywords</label>
-            <input type="text" onChange={(event) => setKeywords(event.target.value)} id="listKeyword" name="listKeyword" placeholder="Flight" />
-            <button onClick={() => newKeyword()}>Add Keyword</button>
-            {keywords.map((keyword, index) => {
+            <input type="text" onChange={(event) => setKeywordStr(event.target.value)} id="listKeyword" name="listKeyword" placeholder="Flight" required />
+            <button onClick={() => newKeyword(keywordStr)}>Add Keyword</button>
+            {keywords.map((item, index) => {
                 return (
                     <div>
-                        <li key={index}>{keyword}</li>
+                        <li key={index}>{item}</li>
                     </div>
                 )
             })}
             <h2>Add List Items</h2>
             <label for="itemName">Name</label>
-            <input type="text" onChange={nameChangeHandler} id="itemName" name="itemName" placeholder="Fly to the Moon" />
+            <input type="text" onChange={nameChangeHandler} id="itemName" name="itemName" placeholder="Fly to the Moon" required />
             <label for="itemInfo">Info</label>
-            <input type="text" onChange={infoChangeHandler} id="itemInfo" name="itemInfo" placeholder="Need a Rocket" />
-            <button onClick={() => newListItem()}>Add Item</button>
-            {listItems.map((item1, item2, index) => {
+            <input type="text" onChange={infoChangeHandler} id="itemInfo" name="itemInfo" placeholder="Need a Rocket" required />
+            <button onClick={() => newListItem(itemName, itemInfo)}>Add Item</button>
+            {listItems.map((item, index) => {
                 return (
-                    <div>
-                        <li key={index}>{item1} {item2}</li>
-                    </div>
+                    <ul>
+                        <li key={index}>{item.itemName} - {item.itemInfo}</li>
+                    </ul>
                 )
             })}
+            <button onClick={newListHandler}>Save List</button>
         </div >
     )
 };
-
-// onChange={infoChangeHandler}

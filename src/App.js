7e56fe62bd9fc6076716ce2fc72ac7d0f9a1cc51
Ju1ija <j.lipnickaja@gmail.com>
2022-01-07 10:55:14 +0000

    added react router to app.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
-// import { SignUp } from "./components/signup.js";
+import { SignUp } from "./components/signup.js";
 import { Home } from "./components/home.js";
+import { LogIn } from "./components/logIn.js";
+import { Profile } from "./components/profile.js";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Home />
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/login">
+          <LogIn />
+        </Route>
+        <Route path="/signup">
+          <SignUp />
+        </Route>
+        <Route path="/home">
+          <Home />
+        </Route>
+        <Route path="/profile">
+          <Profile />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 

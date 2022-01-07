575bb106699412f33c3874859fdef387ca2d98a7
rosemarym2 <rosemarymay91@gmail.com>
2022-01-07 16:19:51 +0000

    updated settings page

--- src/components/settings.js
+++ src/components/settings.js
@@ -4,7 +4,6 @@ import { getUserFetch } from "../utils";
 
 export const Settings = (props) => {
   const [user, setUser] = useState("");
-//   const [list, setList] = useState([]);
   const getUser = async () => {
     const profile = await getUserFetch("61d5ace72b3bb16c099a7b29");
     console.log(profile);
@@ -23,21 +22,27 @@ export const Settings = (props) => {
         Img="https://res.cloudinary.com/cn-project/image/upload/v1641488639/pana/Binary_code-pana_ld9rm6.png"
         username={user} // user profile details linked with the back-end database
       />
-      <Categories
+      <EditProfile
       title = "Edit Profile"
       />
-      <Categories
+      <Colour
       title = "Colour Scheme"
       colour1= "light"
       colour2= "dark"
       />
-      <Categories
+      <FontSize
       title = "Font Size"
+      sizeSmall = "small"
+      sizeMedium = "medium"
+      sizeLarge = "large"
       />
-      <Categories
+      <ListStyle
       title = "List Style"
+      scratch = "scratchcard"
+      flip = "card flip"
+      checklist= "checklist"
       />
-      <SubCategories
+      <Delete
       title = "DELETE PROFILE"
       />
     </div>
@@ -48,17 +53,14 @@ const UserProfile = (props) => {
   return (
     <div className="UserProfile">
       <img src={props.Img} className="Img" />
-      <img src={props.Img2} className="Img" />
-      <img src={props.Img3} className="Img" />
       <h2>{props.username}</h2>
     </div>
   );
 };
 
-const Categories = (props) => {
+const Colour = (props) => {
   return (
-    <div className="Article">
-      <img src={props.Img} className="Img" />
+    <div className="Colour">
       <button>{props.title}</button>
       <SubCategories
       title = {props.colour1}
@@ -66,16 +68,63 @@ const Categories = (props) => {
       <SubCategories
       title = {props.colour2}
       />
-      <SubCategories
-      />
     </div>
   );
 };
 
+const FontSize = (props) => {
+    return (
+      <div className="FontSize">
+        <button>{props.title}</button>
+        <SubCategories
+        title = {props.sizeSmall}
+        />
+        <SubCategories
+        title = {props.sizeMedium}
+        />
+        <SubCategories
+        title = {props.sizeLarge}
+        />
+      </div>
+    );
+  };
+
+  const ListStyle = (props) => {
+    return (
+      <div className="ListStyle">
+        <button>{props.title}</button>
+        <SubCategories
+        title = {props.scratch}
+        />
+        <SubCategories
+        title = {props.flip}
+        />
+        <SubCategories
+        title = {props.checklist}
+        />
+      </div>
+    );
+  };
+
+const EditProfile = (props) => {
+    return (
+      <div className="EditProfile">
+        <button>{props.title}</button>
+      </div>
+    );
+  };
+
 const SubCategories = (props) => {
     return (
-      <div className="Article">
-        <img src={props.Img} className="Img" />
+      <div className="Subcategories">
+        <button>{props.title}</button>
+      </div>
+    );
+  };
+
+  const Delete = (props) => {
+    return (
+      <div className="Delete">
         <button>{props.title}</button>
       </div>
     );

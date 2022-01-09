b9855b2929ead09d87a4d7b8c84c67dbc7d99369
Ju1ija <j.lipnickaja@gmail.com>
2022-01-09 16:38:52 +0000

    added images and some css

--- src/components/category.js
+++ src/components/category.js
@@ -1,10 +1,14 @@
 import { useState, useEffect } from "react";
 import { getFilteredListsFetch } from "../utils";
-import { useParams } from "react-router-dom";
+import { Switch, useParams } from "react-router-dom";
+import "./category.css";
 
 export const Category = () => {
   const { handle } = useParams();
   const [listArr, setListArr] = useState([]);
+  const [listImg, setListImg] = useState("");
+
+
 
   useEffect(() => {
     dataHandler(handle);
@@ -13,19 +17,35 @@ export const Category = () => {
   const dataHandler = async (handle) => {
     const data = await getFilteredListsFetch("category", handle);
     setListArr(data.filteredLists);
-    console.log(listArr);
+    switch (handle) {
+      case "Travel":
+        setListImg("https://res.cloudinary.com/cn-project/image/upload/v1641481525/pana/Winter_solstice-pana_x4wbwx.png")
+        break;
+      case "Music":
+        setListImg("https://res.cloudinary.com/cn-project/image/upload/v1641481525/pana/Headphone-pana_ay39wz.png");
+        break;
+      case "Books":
+        setListImg("https://res.cloudinary.com/cn-project/image/upload/v1641481525/pana/Book_lover-pana_gvn56e.png");
+        break;
+      case "Movies":
+        setListImg("https://res.cloudinary.com/cn-project/image/upload/v1641481525/pana/Film_rolls-pana_wipspq.png");
+        break;
+    }
   }
 
   return (
-    <div>
+    <div className="category-component">
       <h1>{handle}</h1>
-      {listArr.map((item, index) => {
-        return (
-          <div key={index}>
-            <p>{item.title}</p>
-          </div>
-        )
-      })}
+      <div className="category-all-items">
+        {listArr.map((item, index) => {
+          return (
+            <div key={index} className="category-item">
+              <img src={listImg} className="category-image" />
+              <p>{item.title}</p>
+            </div>
+          )
+        })}
+      </div>
     </div>
   );
 }

8e1533ffb9b2c86e7ae3948089eb338d201cd6f7
Ju1ija <j.lipnickaja@gmail.com>
2022-01-09 17:46:02 +0000

    updated public list images

--- src/components/publicList.js
+++ src/components/publicList.js
@@ -1,14 +1,12 @@
 import { useParams } from "react-router-dom";
 import { useState, useEffect } from "react";
 import { getSpecificListFetch } from "../utils";
-import grey from "../images/grey.png";
-import travel from "../images/travel.png";
-import ScratchCard from 'react-scratchcard';
 
 export const List = () => {
   const { handle } = useParams();
   const [listTitle, setListTitle] = useState("");
   const [items, setItems] = useState([]);
+  const [listImg, setListImg] = useState("");
 
   useEffect(() => {
     dataHandler(handle);
@@ -19,14 +17,20 @@ export const List = () => {
     console.log(data);
     setListTitle(data.list.title);
     setItems(data.list.listItems);
-  }
-
-  const settings = {
-    width: 150,
-    height: 150,
-    image: grey,
-    finishPercent: 80,
-    onComplete: () => console.log("item complete")
+    switch (data.list.category) {
+      case "Travel":
+        setListImg("https://res.cloudinary.com/cn-project/image/upload/v1641481525/pana/Winter_solstice-pana_x4wbwx.png")
+        break;
+      case "Music":
+        setListImg("https://res.cloudinary.com/cn-project/image/upload/v1641481525/pana/Headphone-pana_ay39wz.png");
+        break;
+      case "Books":
+        setListImg("https://res.cloudinary.com/cn-project/image/upload/v1641481525/pana/Book_lover-pana_gvn56e.png");
+        break;
+      case "Movies":
+        setListImg("https://res.cloudinary.com/cn-project/image/upload/v1641481525/pana/Film_rolls-pana_wipspq.png");
+        break;
+    }
   }
 
   return (
@@ -36,13 +40,7 @@ export const List = () => {
         {items.map((item, index) => {
           return (
             <div className="card" key={index}>
-              {item.completed == false ? (
-                <ScratchCard {...settings}>
-                  <img src={travel} style={{ width: "150px" }} />
-                </ScratchCard>
-              ) : (
-                <img src={travel} style={{ width: "150px" }} />
-              )}
+              <img src={listImg} style={{ width: "150px" }} />
               <h5 style={{ margin: "5px" }}>{item.itemName}</h5>
               <p style={{ fontSize: "12px", margin: "0" }}>{item.itemInfo}</p>
             </div>

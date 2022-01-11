a924897bf5033179dac6fed5a751386e4a41a9ce
Rachel Groves <grovrac09@gmail.com>
2022-01-11 11:31:20 +0000

    pulled changes

--- src/components/profile/profile.js
+++ src/components/profile/profile.js
@@ -36,22 +36,6 @@ export const Profile = () => {
   }
 
   return (
-<<<<<<< HEAD
-    <div className="profileBody">
-      <div className="userProfile">
-        <h1>My Collection</h1>
-        <UserProfile
-          Img="https://res.cloudinary.com/cn-project/image/upload/v1641488639/pana/Binary_code-pana_ld9rm6.png"
-          // Img2 = "https://res.cloudinary.com/cn-project/image/upload/v1641486239/pana/Self_confidence-pana_zo0elk.png"
-          // Img3 = "https://res.cloudinary.com/cn-project/image/upload/v1641486240/pana/404_Error_with_a_cute_animal-pana_uumdxx.png"
-          // username = {`${user.username}`} //need to link user profile details with the back-end database
-          username={user} //need to link user profile details with the back-end database
-        />
-        <h2>In Progress</h2>
-        <div className="prog">
-          {inProgress.map((item, index) => {
-            return (
-=======
     <div className="userProfile">
       <TopNav />
       <h1>My Collection</h1>
@@ -70,22 +54,12 @@ export const Profile = () => {
               <div className="delete-personal-list-button" onClick={() => deleteHandler(item._id)}>
                 <i class="bi bi-trash"></i>
               </div>
->>>>>>> 2cad4ce8011966eb21be0ae603df909d3e34e0b7
               <Link to={`/profile/lists/${item._id}`}>
                 <div key={index}>
                   <img src={item.listImage} style={{ width: "150px" }} />
                   <h5>{item.title}</h5>
                 </div>
               </Link>
-<<<<<<< HEAD
-            )
-          })}
-        </div>
-        <h2>Completed</h2>
-        <div className="comp">
-          {completed.map((item, index) => {
-            return (
-=======
             </div>
           )
         })}
@@ -98,22 +72,12 @@ export const Profile = () => {
               <div className="delete-personal-list-button" onClick={() => deleteHandler(item._id)}>
                 <i class="bi bi-trash"></i>
               </div>
->>>>>>> 2cad4ce8011966eb21be0ae603df909d3e34e0b7
               <Link to={`/profile/lists/${item._id}`}>
                 <div key={index}>
                   <img src={item.listImage} style={{ width: "150px" }} />
                   <h5>{item.title}</h5>
                 </div>
               </Link>
-<<<<<<< HEAD
-            )
-          })}
-        </div>
-        <h2>Saved</h2>
-        <div className="save">
-          {saved.map((item, index) => {
-            return (
-=======
             </div>
           )
         })}
@@ -126,28 +90,12 @@ export const Profile = () => {
               <div className="delete-personal-list-button" onClick={() => deleteHandler(item._id)}>
                 <i class="bi bi-trash"></i>
               </div>
->>>>>>> 2cad4ce8011966eb21be0ae603df909d3e34e0b7
               <Link to={`/profile/lists/${item._id}`}>
                 <div key={index}>
                   <img src={item.listImage} style={{ width: "150px" }} />
                   <h5>{item.title}</h5>
                 </div>
               </Link>
-<<<<<<< HEAD
-            )
-          })}
-        </div>
-        <h2>Create Your Own</h2>
-        <div className="create">
-          <Link to="/newList">
-            <CreateList
-              Img="https://res.cloudinary.com/cn-project/image/upload/v1641486603/pana/No_data-pana_f82ggb.png"
-              alt="Animated picture of person with an empty list"
-              title="New List"
-            />
-          </Link>
-        </div>
-=======
             </div>
           )
         })}
@@ -161,7 +109,6 @@ export const Profile = () => {
             title="New List"
           />
         </Link>
->>>>>>> 2cad4ce8011966eb21be0ae603df909d3e34e0b7
       </div>
     </div>
   );

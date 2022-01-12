8cd1d28054295ba7ebc21acf54cb4d3e14bc5418
Holly Ford <holly_ford@hotmail.com>
2022-01-12 16:01:24 +0000

    styled add new list page

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -8,7 +8,6 @@ export const About = () => {
         <div>
             <TopNav />
         <div className="about">
-            <h1>About us</h1>
             <img className="aboutImg" src="https://res.cloudinary.com/cn-project/image/upload/v1641917408/pana/misc/FAQs-pana_h4mv1y.png"></img>
             <h3>What is List Junkie?</h3>
             <h5>This is a space for you to go wild with your bucket lists! Always wanted to sky dive? Visit the Amazon? Have the bragging rights of reading all of the classics? Show off all the awesome thing's you've done and plan to do? Have fun with our scratch off lists! Use our existing lists or create your own! 
